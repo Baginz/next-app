f77d5328ad23da4aa0e79bd975d8c0db195c2bcb
Baginz <baginsqt@gmail.com>
2022-01-09 22:08:27 +0300

    feat: add Button, Htag, P, Tag components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
+import React from "react";
 import type { NextPage } from "next";
-import Image from "next/image";
-
+import Htag from "../components/Htag/Htag";
+import Button from "../components/Button/Button";
+import { Tag } from "../components/Tag/Tag";
+import { P } from "../components/P/P";
 
 const Home: NextPage = () => {
     return (
-        <div >
-           
-        </div>
+        <>
+            <Htag tag="h1">Text</Htag>
+            <Button appearance="primary" arrow='right'>button</Button>
+            <Button appearance="ghost" arrow='right'>button</Button>
+            <P>fgfgfg</P>
+            <Tag size="m" color="primary">wdwdwd</Tag>
+            <Tag size="s" color="ghost">wdwdwd</Tag>
+            <Tag size="s" color="green">wdwdwd</Tag>
+            <Tag size="s" color="red">wdwdwd</Tag>
+        </>
     );
 };
 
